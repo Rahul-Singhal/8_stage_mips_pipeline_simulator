299ba1c611d34ae8ab5e07d4792ddfbdc0a20b08
VED RATN DIXIT <vrd@nsl-34.cse.iitb.ac.in>
2013-10-26 01:02:42 +0530

    First commit ved

---
+++ Instruction.h
@@ -0,0 +1,13 @@
+#ifndef INSTR_H
+#define INSTR_H
+
+#include "Register.h"
+#include <vector>
+
+class Instruction{
+public:
+	static vector <Register> registers;
+	Instruction();
+};
+
+#endif
---
+++ Register.cpp
@@ -0,0 +1,7 @@
+#include "Register.h"
+
+Register::Register(int id, int value){
+	this->id = id;
+	this->value = value;
+	this->status = true;
+}
